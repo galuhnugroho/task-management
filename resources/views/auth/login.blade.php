84f113d0075f777207bd70c5b096d228d8f7ad5f
Galuh <gitgaluh@gmail.com>
2025-03-04 07:20:09 +0700

    style(ui): update user name & logout button layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,16 +8,25 @@
                     <h3 class="m-0 text-center">Login</h3>
                 </div>
                 <div class="card-body">
+                    @if (session('failed'))
+                        <div class="alert alert-danger text-center">{{ session('failed') }}</div>
+                    @endif
                     <form action="/login" method="POST">
                         @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
                             <input type="email" name="email" class="form-control" id="email">
                         </div>
+                        @error('email')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
                             <input type="password" name="password" class="form-control" id="password">
                         </div>
+                        @error('password')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
                         <div class="mb-3 form-check">
                             <input type="checkbox" name="remember" class="form-check-input" id="remember">
                             <label class="form-check-label" for="remember">Remember me</label>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,26 +1,40 @@
 @extends('app')
 
 @section('content')
+    <div class="d-flex justify-content-end m-3">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="nav-icon fa-solid fa-user"></i>
+                {{ auth()->user()->name }}
+            </button>
+            <ul class="dropdown-menu center">
+                <li>
+                    <a href="/logout" class="dropdown-item text-center">
+                        <i class="nav-icon fa-solid fa-power-off"></i>
+                        Logout
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
     <div class="container mt-5">
         <div class="col-md-12">
             <h2>Task Management</h2>
         </div>
 
-        <div class="col-md-12">
-            <a href="/logout" class="btn btn-danger">Logout</a>
-        </div>
 
 
         @session('success')
             <div class="col-md-12">
-                <div class="alert  alert-success">
+                <div class="alert alert-success">
                     {{ session('success') }}
                 </div>
             </div>
         @endsession
 
         <div class="col-md-12">
-            <a href="{{ route('tasks.create') }}" class="btn btn-primary shadow mb-2">Tambah Task</a>
+            <a href="{{ route('tasks.create') }}" class="btn btn-sm btn-primary shadow mb-2">Tambah Task</a>
         </div>
 
         <div class="col-md-12">
@@ -45,20 +59,21 @@
 
                             <tbody>
                                 @foreach ($tasks as $task)
-                                    <tr>
+                                    <tr class="text-center">
                                         <td>{{ $loop->iteration }}</td>
                                         <td>{{ $task->title }}</td>
                                         <td>{{ Str::limit($task->description, 50, '...') }}</td>
                                         <td>{{ $task->is_completed ? 'Selesai' : 'Belum' }}</td>
                                         <td>
                                             <a href="{{ route('tasks.edit', $task->slug) }}"
-                                                class="btn btn-warning">Edit</a>
-                                            <a href="{{ route('tasks.show', $task->slug) }}" class="btn btn-info">Show</a>
+                                                class="btn btn-sm btn-warning">Edit</a>
+                                            <a href="{{ route('tasks.show', $task->slug) }}"
+                                                class="btn btn-sm btn-info">Show</a>
                                             <form action="{{ route('tasks.destroy', $task->id) }}" method="POST"
                                                 class="d-inline">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" class="btn btn-danger"
+                                                <button type="submit" class="btn btn-sm btn-danger"
                                                     onclick="Yakin, ingin menghapus?">Delete</button>
                                             </form>
                                         </td>
